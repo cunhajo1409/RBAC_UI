359b5b572b48765934cb0b5aa51a31a33df32c1d
jcunha-spinsys <jcunha@spinsys.com>
2022-04-18 16:20:02 -0400

    Intial Push of version with grids to AVHE API

---
+++ RBAC_Site/Sites/RBAC/SiteAssets/js/Globals.js
@@ -0,0 +1,12 @@
+let runEnvironment = "local";
+let imageFolderRelativePath = 'Sites/RBAC/SiteAssets/image/';
+
+let ADUsersArray = new Array();
+let ADGroupsArray = new Array();
+let ADUsersGroupsArray = new Array();
+let ADUserAccountTypesArray = new Array();
+let MIPGroupProjectsArray = new Array();
+let MIPGroupScopesArray = new Array();
+let MIPGroupTypesArray = new Array();
+
+
