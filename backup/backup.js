d84f9eed15d87750efbd407a1a2d7c71b9b65b05
jcunha-spinsys <jcunha@spinsys.com>
2022-07-14 13:20:21 -0400

    All Grids move to Menu Selection, Deactivate functions completed, title toggled for appropriate menu

---
+++ backup/backup.js
@@ -0,0 +1,63 @@
+$(".k-grid-save-changes").click(function()
+{
+    console.log("update selected")
+})
+
+            //var grpGrid = $("#divGrdADGroups").data("kendoGrid");
+            //var grpdataSource = grpGrid.dataSource;
+            // grpdataSource.read();
+            // grpGrid.refresh();
+            
+            //var pager = $("#pager").kendoPager({dataSource:grpdataSource}).data("kendoPager");
+            //grpGrid.pager.refresh();
+
+            // var grpSort = [];
+            // grpSort.push({field: "groupKey", dir: "asc"});
+            // grpGrid.dataSource.sort(grpSort);
+            // grpGrid.refresh();
+            // function onChange(arg)
+            // {
+            //     console.log("entered on change");
+            //     var selected = $.map(this.select(), function(item)
+            //     {
+            //         return $(item).text();
+            //     });
+
+            //     console.log("Selected: " + selected.length + " item(s), [" + selected.join(", ") + "]");
+            // }
+
+            // function onDataBound(arg) {
+            //     console.log("Grid data bound");
+            // }
+
+            // function onDataBinding(arg) {
+            //     console.log("Grid data binding");
+            // }
+
+            // function onSorting(arg) {
+            //     console.log("Sorting on field: " + arg.sort.field + ", direction:" + (arg.sort.dir || "none"));
+            // }
+
+            // function onFiltering(arg) {
+            //     console.log("Filter on " + kendo.stringify(arg.filter));
+            // }
+
+            // function onPaging(arg) {
+            //     console.log("Paging to page index:" + arg.page);
+            // }
+
+            // function onGrouping(arg) {
+            //     console.log("Group on " + kendo.stringify(arg.groups));
+            // }
+
+            // function onGroupExpand(arg) {
+            //     console.log("The group to be expanded: " + kendo.stringify(arg.group));
+            // }
+
+            // function onGroupCollapse(arg) {
+            //     console.log("The group to be collapsed: " + kendo.stringify(arg.group));
+            // }  
+
+
+            // console.log($("#divGrdADGroups table").first().children("thead").children("tr").children(".k-header").children(".k-link").first());
+            // $("#divGrdADGroups table").first().children("thead").children("tr").children(".k-header").children(".k-link").first().children("span").click();
